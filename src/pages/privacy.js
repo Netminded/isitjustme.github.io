3caf3fc4352c3cdfbac087337597bef56ce59874
Tam Love <studio@tamlove.com>
2024-11-11 15:54:54 +0000

    Added breaklines.

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -209,6 +209,9 @@ const Privacy = () => {
 
                             <h4 className="text-accent">Third Party Information</h4>
                             <p>The use of information received from Google APIs will adhere to the Chrome Web Store User Data Policy, including the Limited Use requirements.</p>
+                            
+                            <br />
+                            <br />
 
                             <h4 className="text-accent">Changes to our privacy policy</h4>
 
